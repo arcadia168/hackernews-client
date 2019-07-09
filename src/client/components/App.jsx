0c55d3adf2b90fbb0c3a82212097a7aaa054bf09
Erin Eland <erin@erineland.com>
2019-07-09 23:45:42 +0100

    Successfully calling down and rendering the initial 30 top news items.

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -2,20 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Button } from 'react-bootstrap';
 import PropTypes from 'prop-types'
-
-const HelloWorld = () => <h1 data-testid="app-title" className="app__title">Hello World</h1>;
+import TopHackerNewsItems from './TopHackerNewsItems.jsx';
 
 const App = () => {
   return (
     <Router>
-      <Route exact path="/" component={HelloWorld} />
-      <Button>Hello React Start with Bootstrap!</Button>
+      <Route exact path="/" component={TopHackerNewsItems} />
     </Router>
   )
-}
-
-App.propTypes = {
-  axiosInstance: PropTypes.func,
 }
 
 export default App;
