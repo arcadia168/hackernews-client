b73132e7b7ace38c7812250ba630d4911ab6acf7
Erin Eland <erin@erineland.com>
2019-07-10 01:49:30 +0100

    Got basic nested comments loading asynchronously

---
+++ src/client/components/HackerNewsComment.jsx
@@ -0,0 +1,111 @@
+// {
+//     "by" : "norvig",
+//     "id" : 2921983,
+//     "kids" : [ 2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141 ],
+//     "parent" : 2921506,
+//     "text" : "Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?",
+//     "time" : 1314211127,
+//     "type" : "comment"
+//   }
+
+import React, { Component } from 'React';
+import { Row, Alert, Spinner, Button } from 'react-bootstrap';
+import moment from 'moment';
+import axios from 'axios';
+
+// TODO: Use a pure functional component if no state changes...
+export default class HackerNewsComment extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            childCommentDetails: [],
+            loading: false,
+            error: undefined
+        }
+        this.loadChildCommentDetails = this.loadChildCommentDetails.bind(this);
+        this.hackersNewsApiBaseUrl = 'https://hacker-news.firebaseio.com/v0';
+
+    }
+
+    loadChildCommentDetails() {
+         // Make a call to get the next 30 news items using the appropriate item IDs
+         debugger;
+
+         // Set the state to be loading and show the spinner
+         this.setState({
+             loading: true
+         })
+ 
+         const childCommentDetailsCalls = [];
+         for (let i = 0; i < this.props.commentDetails.kids.length; i++) {
+             const currentChildCommentId = this.props.commentDetails.kids[i];
+             console.info(`The currentChildCommentId about to have details requested is: ${currentChildCommentId}`);
+             childCommentDetailsCalls.push(axios.get(`${this.hackersNewsApiBaseUrl}/item/${currentChildCommentId}.json`));
+         }
+ 
+         // debugger;
+         axios.all(childCommentDetailsCalls).then(childCommentDetailsResponses => {
+             // debugger;
+             console.info(`The child comment details are: ${JSON.stringify(childCommentDetailsResponses)}`);
+ 
+             // Iterate through responses, extracting out the data and assign these all to the state 
+             const childCommentDetailsData = [];
+             childCommentDetailsResponses.forEach(currentChildCommentDetailsResponse => {
+                childCommentDetailsData.push(currentChildCommentDetailsResponse.data)
+             });
+ 
+             // debugger;
+             this.setState({
+                 childCommentDetails: childCommentDetailsData,
+                 loading: false
+             });
+         }).catch(error => {
+             // debugger;
+             console.error(`Something went wrong attempting to fetch the first 30 news item details: ${JSON.stringify(error)}`);
+ 
+             // Set the error on the state and display it to the user in an alert
+             this.setState({
+                 error: error.message
+             });
+         });
+    }
+
+    render() {
+        console.info(`In hacker news item, the props are: ${JSON.stringify(this.props)}`);
+
+        const momentTimePosted = moment.unix(this.props.commentDetails.time).format('DD/MM/YYYY HH:mm');
+        console.info(`The momentTimePosted (in hours): ${momentTimePosted}`);
+
+        debugger;
+
+        return (
+            <Row className="hacker-news-comment__container">
+                <p className="hacker-news-comment__test">{this.props.commentDetails.text}</p>
+                <p className="hacker-news-comment__description">Posted by {this.props.commentDetails.by} at {momentTimePosted}</p>
+                {
+                    this.state.error ?
+                        <Alert variant="danger">
+                            {`Something went wrong loading comments: ${this.state.error}`}
+                        </Alert>
+                        : 
+                        this.state.loading ? 
+                            <Row className="top-hacker-news-items__spinner">
+                                <Spinner animation="border" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </Spinner>
+                            </Row>
+                            :
+                            this.state.childCommentDetails ?
+                                this.state.childCommentDetails.map(currentChildCommentDetails => {
+                                    debugger;
+                                    return (
+                                    <HackerNewsComment commentDetails={currentChildCommentDetails} />
+                                    )
+                                })
+                                :
+                                <Button onClick={this.loadChildCommentDetails}>Load child comments...</Button>
+                }
+            </Row>
+        );
+    }
+}
--- src/client/components/HackerNewsItem.jsx
+++ src/client/components/HackerNewsItem.jsx
@@ -1,12 +1,63 @@
 import React, { Component } from 'React';
-import { Row } from 'react-bootstrap';
+import { Row, Alert, Button, Spinner } from 'react-bootstrap';
+import axios from 'axios';
 import moment from 'moment';
+import HackerNewsComment from './HackerNewsComment.jsx';
 
 // TODO: Use a pure functional component if no state changes...
 export default class HackerNewsItem extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loading: false,
+            commentDetails: undefined
+        }
+        this.loadComments = this.loadComments.bind(this);
+        this.hackersNewsApiBaseUrl = 'https://hacker-news.firebaseio.com/v0';
     }
+
+    loadComments() {
+        // Make a call to get the next 30 news items using the appropriate item IDs
+        debugger;
+
+        // Set the state to be loading and show the spinner
+        this.setState({
+            loading: true
+        })
+
+        const commentDetailsCalls = [];
+        for (let i = 0; i < this.props.itemDetails.kids.length; i++) {
+            const currentCommentId = this.props.itemDetails.kids[i];
+            console.info(`The currentCommentId about to have details requested is: ${currentCommentId}`);
+            commentDetailsCalls.push(axios.get(`${this.hackersNewsApiBaseUrl}/item/${currentCommentId}.json`));
+        }
+
+        // debugger;
+        axios.all(commentDetailsCalls).then(commentDetailsResponses => {
+            // debugger;
+            console.info(`The child comment details are: ${JSON.stringify(commentDetailsResponses)}`);
+
+            // Iterate through responses, extracting out the data and assign these all to the state 
+            const commentDetailsData = [];
+            commentDetailsResponses.forEach(currentCommentDetailsResponse => {
+                commentDetailsData.push(currentCommentDetailsResponse.data)
+            });
+
+            debugger;
+            this.setState({
+                commentDetails: commentDetailsData,
+                loading: false
+            });
+        }).catch(error => {
+            // debugger;
+            console.error(`Something went wrong attempting to fetch the first 30 news item details: ${JSON.stringify(error)}`);
+
+            // Set the error on the state and display it to the user in an alert
+            this.setState({
+                error: error.message
+            });
+        });
+   }
 
     render() {
         console.info(`In hacker news item, the props are: ${JSON.stringify(this.props)}`);
@@ -18,6 +69,29 @@ export default class HackerNewsItem extends Component {
             <Row className="hacker-news-item__container">
                 <h5 className="hacker-news-item__title"><a href={this.props.itemDetails.url}>{this.props.itemDetails.title}</a></h5>
                 <p className="hacker-news-item__description">{this.props.itemDetails.score} by {this.props.itemDetails.by} at {momentTimePosted}</p>
+                {
+                    this.state.error ?
+                        <Alert variant="danger">
+                            {`Something went wrong loading comments: ${this.state.error}`}
+                        </Alert>
+                        : 
+                        this.state.loading ? 
+                            <Row className="top-hacker-news-items__spinner">
+                                <Spinner animation="border" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </Spinner>
+                            </Row>
+                            :
+                            this.state.commentDetails ?
+                                this.state.commentDetails.map(currentChildCommentDetails => {
+                                    debugger;
+                                    return (
+                                        <HackerNewsComment commentDetails={currentChildCommentDetails} />
+                                    )
+                                })
+                                :
+                                <Button onClick={this.loadComments}>Show comments</Button>
+                }
             </Row>
         );
     }
